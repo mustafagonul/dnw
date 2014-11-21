f0b243a9012129414aaab24885cf5365665e6112
Mustafa Gönül <mustafagonul@gmail.com>
2014-11-21 20:46:05 +0200

    - eclipse script has been updated.

--- eclipse.sh
+++ eclipse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir ../dnw_debug
-cd ../dnw_debug
-cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../dnw
+cd ../dnw_debug 
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.4 -DCMAKE_CXX_COMPILER_ARG1=-std=c++14 -DCMAKE_BUILD_TYPE=Debug  ../dnw
