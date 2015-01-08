82834a58fa3752040f5966de7643e683585ddc98
Mustafa Gönül <mustafagonul@gmail.com>
2015-01-08 21:14:12 +0200

    - Improved deploy (7).

--- build.sh
+++ build.sh
@@ -8,9 +8,14 @@
 # and run it :)
 
 git clone https://github.com/mustafagonul/dnw.git
+
 mkdir dnw_release
 cd dnw_release/
 cmake -DCMAKE_BUILD_TYPE=Release ../dnw
 make -j$(nproc)
 make install
+
+cd ..
+rm -R dnw
+rm -R dnw_release
 
