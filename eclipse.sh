e9a82d652d1ed5a0ba2745ed9abc27d6e7571615
Mustafa Gönül <mustafagonul@gmail.com>
2015-01-14 18:19:41 +0200

    - Eclipse project generating script from cmake, improved.

--- eclipse.sh
+++ eclipse.sh
@@ -2,4 +2,6 @@
 
 mkdir ../dnw_debug
 cd ../dnw_debug 
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.4 -DCMAKE_CXX_COMPILER_ARG1=-std=c++14 -DCMAKE_BUILD_TYPE=Debug  ../dnw
+
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.4 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_CXX_COMPILER_ARG1=-std=c++14 -DCMAKE_BUILD_TYPE=Debug  ../dnw
+# -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8
